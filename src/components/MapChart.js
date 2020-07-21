211921ddc9142e37e5aaa166ac8270d2bdf01ae1
Jacob Fitzhugh <MacZero@Jacobs-MacBook-Pro.local>
2020-07-21 10:48:24 -0400

    added sphere, graticule, csv and scaleLinear

--- src/components/MapChart.js
+++ src/components/MapChart.js
@@ -1,9 +1,13 @@
-import React, { memo } from "react";
+import React, { memo, useEffect, useState } from "react";
+import { csv } from "d3-fetch";
+import { scaleLinear } from "d3-scale";
 import {
   ZoomableGroup,
   ComposableMap,
   Geographies,
   Geography,
+  Sphere,
+  Graticule,
 } from "react-simple-maps";
 
 const geoUrl =
@@ -19,49 +23,74 @@ const rounded = (num) => {
   }
 };
 
+const colorScale = scaleLinear()
+  .domain([0.29, 0.68])
+  .range(["#03041e", "#d00000"]);
+
 function handleClick(e) {
   console.log("that tickles");
 }
 
 const MapChart = ({ setTooltipContent }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    csv(`/vulnerability.csv`).then((data) => {
+      setData(data);
+    });
+  }, []);
+
   return (
     <>
-      <ComposableMap data-tip="" projectionConfig={{ scale: 200 }}>
+      <ComposableMap
+        data-tip=''
+        projectionConfig={{
+          rotate: [-10, 0, 0],
+          scale: 200,
+        }}
+      >
         <ZoomableGroup>
-          <Geographies geography={geoUrl}>
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  onMouseEnter={() => {
-                    const { NAME, POP_EST } = geo.properties;
-                    setTooltipContent(`${NAME} — ${rounded(POP_EST)}`);
-                  }}
-                  onMouseLeave={() => {
-                    setTooltipContent("");
-                  }}
-                  // onClick={() => {
-                  //
-                  // }}
-                  style={{
-                    default: {
-                      fill: "#D6D6DA",
-                      outline: "none",
-                    },
-                    hover: {
-                      fill: "#F53",
-                      outline: "none",
-                    },
-                    pressed: {
-                      fill: "#E42",
-                      outline: "none",
-                    },
-                  }}
-                />
-              ))
-            }
-          </Geographies>
+          <Sphere stroke='#E4E5E6' strokeWidth={0.5} />
+          <Graticule stroke='#E4E5E6' strokeWidth={0.5} />
+          {data.length > 0 && (
+            <Geographies geography={geoUrl}>
+              {({ geographies }) =>
+                geographies.map((geo) => {
+                  const d = data.find((s) => s.ISO3 === geo.properties.ISO_A3);
+                  return (
+                    <Geography
+                      key={geo.rsmKey}
+                      geography={geo}
+                      onMouseEnter={() => {
+                        const { NAME, POP_EST } = geo.properties;
+                        setTooltipContent(`${NAME} — ${rounded(POP_EST)}`);
+                      }}
+                      onMouseLeave={() => {
+                        setTooltipContent("");
+                      }}
+                      // onClick={() => {
+                      //
+                      // }}
+                      style={{
+                        default: {
+                          fill: d ? colorScale(d["2017"]) : "#4a4e69",
+                          outline: "none",
+                        },
+                        hover: {
+                          fill: "#d00000",
+                          outline: "none",
+                        },
+                        pressed: {
+                          fill: "#ffba08",
+                          outline: "none",
+                        },
+                      }}
+                    />
+                  );
+                })
+              }
+            </Geographies>
+          )}
         </ZoomableGroup>
       </ComposableMap>
     </>
