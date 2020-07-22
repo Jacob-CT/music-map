3a4235c73cfa96f6bc1eeae45896acd8efd6a2fd
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-07-22 09:08:12 -0400

    added boilerplate code

---
+++ src/components/MapMusicContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import MapChart from "./MapChart";
+import Player from "./Player";
+
+const MapMusicContainer = () => {
+  return (
+    <>
+      <MapChart />
+      <Player />
+    </>
+  );
+};
+
+export default MapMusicContainer;
